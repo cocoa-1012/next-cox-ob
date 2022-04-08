d68dfd0b6ab081029332422d5b57bb52356ab5b7
faizan2777 <faizan@attendease.com>
2022-04-09 00:58:53 +0500

    updated files

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+  staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        ...(config.resolve || {}).fallback,
+        fs: false,
+        stream: false,
+        os: false,
+      },
+    };
+
+    // Return the altered config
+    return config;
+  },
+};
